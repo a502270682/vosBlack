2c4a1bd66d931e79388174046b9e691bf1bcd944
feiyangguo <feiyang.guo@iglooinsure.com>
2022-05-14 15:34:36 +0800

    solve conflict

--- model/sys_gateway.go
+++ model/sys_gateway.go
@@ -6,11 +6,11 @@ import (
 	"time"
 )
 
-type SysGateway struct {
+type SysGatewayInfo struct {
 	NID         int       `json:"nid,omitempty" gorm:"column:nID"`
 	GwName      string    `json:"gw_name" gorm:"column:gw_name"`           // 网关名称
 	GwUrl       string    `json:"gw_url" gorm:"column:gw_url"`             //网关调用地址
-	GwType      int       `json:"gw_type" gorm:"column:gw_type"`           // 网关类型
+	GwType      GwType    `json:"gw_type" gorm:"column:gw_type"`           // 网关类型
 	Priority    int       `json:"priority" gorm:"column:priority"`         // 优先级
 	BlackPrefix string    `json:"black_prefix" gorm:"column:black_prefix"` // 接口其他参数
 	IStatus     int       `json:"i_status" gorm:"i_status"`                //状态，0停用，1启动，-1删除
@@ -19,7 +19,7 @@ type SysGateway struct {
 	Remark      string    `json:"remark" gorm:"remark"`                    // 备注
 }
 
-func (SysGateway) TableName() string {
+func (SysGatewayInfo) TableName() string {
 	return "sys_gateway"
 }
 
